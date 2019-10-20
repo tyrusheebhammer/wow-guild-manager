c76d116e89742f0926fb47b8b9dbd53778a250df
Tyrus Sonneborn <tyrus.b@Tyruss-MacBook-Pro.local>
2019-10-20 17:16:44 -0400

    readded files

--- fire-app/src/App.vue
+++ fire-app/src/App.vue
@@ -11,6 +11,9 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #444444;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- fire-app/src/views/polls.vue
+++ fire-app/src/views/polls.vue
@@ -1,15 +1,12 @@
 <template>
-    <div>
-      
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-    name: "Polls"
+  name: 'Polls'
 }
 </script>
 
 <style>
-
 </style>
