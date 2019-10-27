4e1ac353ce0f2ad1840dbd2a03e2487ffc5befbf
phillim <mnphillips42@gmail.com>
2019-10-27 14:26:47 -0400

    Added announcementcard
    Added announcementcard

---
+++ app/src/components/AnnouncementCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <router-link :to="href" class="no-underline">
+        <v-container fill-height class="py-2" >
+            <v-card min-height="110" outlined class="pink-outline">
+                <v-row class="py-3" align-center>
+                    <v-col cols="4" class="py-auto">
+                        <v-img contain :src="imgSrc" class="px-3"></v-img>
+                    </v-col>
+                    <v-col cols="8" class="m-0 pa-0">
+                        <v-row class="m-0 pa-0">
+                        <v-col cols="9" class="m-0 pa-0 stroke">
+                            <v-card-title class="headline mx-1 pa-0 primary--text ">{{ title }}</v-card-title >
+                        </v-col>
+                        </v-row>
+                        <v-row class="m-0 pa-0">
+                        <v-col cols="9" class="m-0 pa-0">
+                            <v-card-subtitle class="mx-1 pa-0 small-line-height">{{ subtitle }}</v-card-subtitle>
+                        </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </v-container>
+    </router-link>
+</template>
+
+<script>
+export default {
+  name: "AnnouncementCard",
+  props: {
+    title: String,
+    subtitle: String
+  }
+};
+</script>
+
+<style>
+    .small-line-height {
+        line-height: 17px !important;
+    }
+
+    .pink-outline {
+        border: 0.5px solid rgb(244, 124, 255) !important;
+        background: #2B2B2B !important;
+    }
+    
+</style>
--- app/src/views/Announcements.vue
+++ app/src/views/Announcements.vue
@@ -1,12 +1,42 @@
 <template>
-    <div>
-
-  </div>
+  <v-container fluid>
+    <v-row dense>
+      <v-col 
+        cols="12" sm="6" 
+        v-for="category in categories"
+        :key="category.title"
+        class="py-0 my-0"
+      >
+        <announcement-card 
+          :title="category.title"
+          :subtitle="category.subtitle"
+        ></announcement-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import AnnouncementCard from '@/components/AnnouncementCard.vue'
 export default {
-  name: 'Announcements'
+  name: 'Announcements',
+  components: {
+    AnnouncementCard
+  },
+  data() {
+    return {
+      categories: [
+        {
+          title: 'Members',
+          subtitle: 'List of guild members'
+        },
+        {
+          title: 'Calendar',
+          subtitle: 'Create and manage events'
+        },
+      ]
+    }
+  }
 }
 </script>
 
