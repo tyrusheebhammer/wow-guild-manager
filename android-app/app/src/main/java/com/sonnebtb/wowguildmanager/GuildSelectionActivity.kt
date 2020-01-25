1579133a5edb16816b6555934a69a88ec1f4c449
Tyrus Sonneborn <tyrus.b@Tyruss-MacBook-Pro.local>
2020-01-25 17:25:51 -0500

    added three new activities

---
+++ android-app/app/src/main/java/com/sonnebtb/wowguildmanager/GuildSelectionActivity.kt
@@ -0,0 +1,12 @@
+package com.sonnebtb.wowguildmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuildSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guild_selection)
+    }
+}
