823e43c6e766a0b12c6d4613415294e5b51350f4
Tyrus Sonneborn <tyrus.b@Tyruss-MacBook-Pro.local>
2020-01-26 22:28:08 -0500

    each fragment is active, added fab to appropriate fragments

--- android-app/app/src/main/java/com/sonnebtb/wowguildmanager/guildinteraction/GuildInteractionActivity.kt
+++ android-app/app/src/main/java/com/sonnebtb/wowguildmanager/guildinteraction/GuildInteractionActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.sonnebtb.wowguildmanager.R
+import com.sonnebtb.wowguildmanager.guildinteraction.announcements.AnnouncementsFragment
+import com.sonnebtb.wowguildmanager.guildinteraction.calendar.CalendarFragment
+import com.sonnebtb.wowguildmanager.guildinteraction.members.MembersFragment
+import com.sonnebtb.wowguildmanager.guildinteraction.polls.PollsFragment
 import kotlinx.android.synthetic.main.activity_guild_interaction.*
 
 class GuildInteractionActivity : AppCompatActivity() {
@@ -15,15 +19,19 @@ class GuildInteractionActivity : AppCompatActivity() {
             var switchTo: Fragment? = null
             val handled = when(it.itemId) {
                 R.id.navigation_announcements -> {
+                    switchTo = AnnouncementsFragment()
                     true
                 }
                 R.id.navigation_members -> {
+                    switchTo = MembersFragment()
                     true
                 }
                 R.id.navigation_calendar -> {
+                    switchTo = CalendarFragment()
                     true
                 }
                 R.id.navigation_polls -> {
+                    switchTo = PollsFragment()
                     true
                 }
                 else -> false
--- android-app/app/src/main/java/com/sonnebtb/wowguildmanager/guildselection/GuildViewHolder.kt
+++ android-app/app/src/main/java/com/sonnebtb/wowguildmanager/guildselection/GuildViewHolder.kt
@@ -1,10 +1,11 @@
-package com.sonnebtb.wowguildmanager
+package com.sonnebtb.wowguildmanager.guildselection
 
 import android.content.Context
 import android.view.View
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.sonnebtb.wowguildmanager.guildselection.GuildAdapter
 import kotlinx.android.synthetic.main.guild_card.view.*
 
 class GuildViewHolder: RecyclerView.ViewHolder {
